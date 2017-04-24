b8a150b7b4740bf4b6e60096069a7ad8d6c9d33f
Avik Mohan <avikbmohan@gmail.com>
2017-04-24 07:30:50 -0700

    Jordan's Create/Load

---
+++ create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Movie(id int, title varchar(100), year int, rating varchar(10), company varchar(50));
+CREATE TABLE Actor(id int, last varchar(20), first varchar(20), sex varchar(6), dob DATE, dod DATE);
+CREATE TABLE Sales(mid int, ticketsSold int, totalIncome int);
+CREATE TABLE Director(id int, last varchar(20), first varchar(20), dob DATE, dod DATE);
+CREATE TABLE MovieGenre(mid int, genre varchar(20));
+CREATE TABLE MovieDirector(mid INT, did INT);
+CREATE TABLE MovieActor(mid int, aid int, role varchar(50));
+CREATE TABLE MovieRating(mid int, imdb int, rot int);
+CREATE TABLE Review(name varchar(20), time TIMESTAMP, mid int, rating int, comment varchar(500));
+CREATE TABLE MaxPersonID(id int);
+CREATE TABLE MaxMovieID(id int);
+
+
