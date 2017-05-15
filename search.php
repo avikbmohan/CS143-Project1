59569dc4fc17af98b7ff7846e41e94cfc0b6208f
cs143 <avikbmohan@gmail.com>
2017-05-15 00:27:36 -0700

    finished search

--- search.php
+++ search.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
++<!DOCTYPE html>
 <html>
   <head>
     <title>
@@ -22,8 +22,13 @@
          exit(1);
        }
        mysql_select_db("CS143", $cxn);
-       
-        $baseQuery = "Select Distinct * from Actor a where a.first like '%".$_GET['query']."%' OR a.last like '%".$_GET['query']."%'";
+       $queryX = explode(" ", $_GET['query']);
+
+       for ($i = 0; $i < count($queryX); $i ++){
+	if ($i == 0){
+          $baseQuery = "Select Distinct * from Actor a where a.first like '%".$queryX[$i]."%' OR a.last like '%".$queryX[$i]."%'"; 
+	} else {
+          $baseQuery = $baseQuery."UNION Select Distinct * from Actor a where a.first like '%".$queryX[$i]."%' OR a.last like '%".$queryX[$i]."%'"; }}
        $res = mysql_query($baseQuery, $cxn);
        $num = mysql_num_fields($res);
        
@@ -46,12 +51,15 @@
 	echo "</tr>";
        }		 
        echo "</table>";
-
-       $baseQuery = "Select Distinct * from Director a where a.first like '%".$_GET['query']."%' OR a.last like '%".$_GET['query']."%'";
+			 
+       for ($i = 0; $i < count($queryX); $i ++){
+	if ($i == 0){
+       $baseQuery = "Select Distinct * from Director a where a.first like '%".$queryX[$i]."%' OR a.last like '%".$queryX[$i]."%'"; 
+	} else {
+          $baseQuery = $baseQuery."UNION Select Distinct * from Director a where a.first like '%".$queryX[$i]."%' OR a.last like '%".$queryX[$i]."%'"; }}
        $res = mysql_query($baseQuery, $cxn);
        $num = mysql_num_fields($res);
        
-
        echo"<br><h4>Directors:<h4><br>";
        echo "<table border='1', cellpadding=4>";
        echo "<tr>";
@@ -76,7 +84,6 @@
        mysql_close($cxn);
 			  }
     ?> 
-
 		      
 		      <h3>Search Movies:</h3>
      <?php 
@@ -91,7 +98,13 @@
        }
        mysql_select_db("CS143", $cxn);
        
-        $baseQuery = "Select Distinct * from Movie a where a.title like '%".$_GET['query1']."%'";
+       $queryX = explode(" ", $_GET['query1']);
+
+       for ($i = 0; $i < count($queryX); $i ++){
+	if ($i == 0){
+          $baseQuery = "Select Distinct * from Movie a where a.title like '%".$queryX[$i]."%'"; 
+	} else {
+          $baseQuery = $baseQuery."UNION Select Distinct * from Movie a where a.title LIKE '%".$queryX[$i]."%'"; }}
        $res = mysql_query($baseQuery, $cxn);
        $num = mysql_num_fields($res);
 
@@ -117,6 +130,107 @@
        mysql_close($cxn);
        }
  ?> 
+
+		      
+		      <h3>Search Actors Directors And Movies:</h3>
+     <?php 
+       echo "<form method=\"get\"> <input type='text' name='query2'> <input type=\"submit\"> </form>";
+       if($_GET["query2"]){
+       $cxn = mysql_connect("localhost", "cs143", "");
+       if(!$cxn){
+         $err = mysql_error($cxn);
+         echo "ERROR!";
+         print("Error: $err");
+         exit(1);
+       }
+       mysql_select_db("CS143", $cxn);
+       
+       $queryX = explode(" ", $_GET['query2']);
+
+       for ($i = 0; $i < count($queryX); $i ++){
+	if ($I == 0){
+          $q1 = "Select Distinct * from Actor a where a.first like '%".$queryX[$i]."%' OR a.last like '%".$queryX[$i]."%'"; 
+          $q2 = "Select Distinct * from Director a where a.first like '%".$queryX[$i]."%' OR a.last like '%".$queryX[$i]."%'"; 
+          $q3 = "Select Distinct * from Movie a where a.title like '%".$queryX[$i]."%'"; 
+	} else {
+          $q1 = $q1."UNION Select Distinct * from Actor a where a.first like '%".$queryX[$i]."%' OR a.last like '%".$queryX[$i]."%'"; 
+	 $q2 = $q2."UNION Select Distinct * from Director a where a.first like '%".$queryX[$i]."%' OR a.last like '%".$queryX[$i]."%'"; 
+          $q3 = $q3."UNION Select Distinct * from Movie a where a.title LIKE '%".$queryX[$i]."%'"; }}
+
+       $res1 = mysql_query($q1, $cxn);
+       $num1 = mysql_num_fields($res1);
+       $res2 = mysql_query($q2, $cxn);
+       $num2 = mysql_num_fields($res2);
+       $res3 = mysql_query($q3, $cxn);
+       $num3 = mysql_num_fields($res3);
+
+       $res = $res1; $num = $num1;
+       echo"<br><h4>Actors:<h4><br>";
+       echo "<table border='1', cellpadding=4>";
+       echo "<tr>";
+       for($i=0; $i < $num; $i++){
+           $fetched = mysql_field_name($res, $i);
+	   echo "<th>"; echo ucfirst(mysql_field_name($res, $i)); echo "</th>";
+       }
+       echo "<th> Link to Bio </th>";
+       echo "</tr>";
+       while($row = mysql_fetch_row($res)){
+        echo "<tr>";
+        for($i = 0; $i < $num; $i++){
+         if (is_null($row[$i])){ echo "<td align=\"center\">---</td>";}
+         else { echo "<td>" . $row[$i] . "</td>";}
+        }
+        echo "<td><center><a href=\"person.php?id=".$row[0]."\"><img src=info.png height=10 width=10></a></center></td>";
+	echo "</tr>";
+       }		 
+       echo "</table>";
+
+       $res = $res2; $num = $num2;
+       echo"<br><h4>Directors:<h4><br>";
+       echo "<table border='1', cellpadding=4>";
+       echo "<tr>";
+       for($i=0; $i < $num; $i++){
+           $fetched = mysql_field_name($res, $i);
+	   echo "<th>"; echo ucfirst(mysql_field_name($res, $i)); echo "</th>";
+       }
+       echo "<th> Link to Bio </th>";
+       echo "</tr>";
+       while($row = mysql_fetch_row($res)){
+        echo "<tr>";
+        for($i = 0; $i < $num; $i++){
+         if (is_null($row[$i])){ echo "<td align=\"center\">---</td>";}
+         else { echo "<td>" . $row[$i] . "</td>";}
+        }
+        echo "<td><center><a href=\"person.php?id=".$row[0]."\"><img src=info.png height=10 width=10></a></center></td>";
+	echo "</tr>";
+       }		 
+       echo "</table>";
+
+
+       $res = $res3; $num = $num3;
+       echo"<br><h4>Movies:<h4><br>";
+       echo "<table border='1', cellpadding=4>";
+       echo "<tr>";
+       for($i=0; $i < $num; $i++){
+           $fetched = mysql_field_name($res, $i);
+	   echo "<th>"; echo ucfirst(mysql_field_name($res, $i)); echo "</th>";
+       }
+       echo "<th> Link to Info </th>";
+       echo "</tr>";
+       while($row = mysql_fetch_row($res)){
+        echo "<tr>";
+        for($i = 0; $i < $num; $i++){
+         if (is_null($row[$i])){ echo "<td align=\"center\">---</td>";}
+         else { echo "<td>" . ($row[$i]) . "</td>";}
+        }
+        echo "<td><center><a href=\"movie.php?id=".$row[0]."\"><img src=info.png height=10 width=10></a></center></td>";
+	echo "</tr>";
+       }		 
+       echo "</table>";
+       mysql_close($cxn);
+       }
+ ?> 
+
 
   </body>
 </html>
